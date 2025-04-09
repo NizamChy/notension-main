19fbada23b0bbae4654566fadf05e72e60c8ee55
NizamChy <nizam.chy.cs@gmail.com>
2025-04-09 18:02:39 +0600

    up

--- src/app/get-mobile-location/page.jsx
+++ src/app/get-mobile-location/page.jsx
@@ -1,10 +1,13 @@
 import React from "react";
-import Map from "@/components/shared/Map/Map";
+// import Map from "@/components/shared/Map/Map";
+import MapMobile from "@/components/shared/Map/MapMobile";
 
 const page = () => {
   return (
     <div className="mt-4 block md:hidden">
-      <Map />
+      {/* <Map /> */}
+
+      <MapMobile />
     </div>
   );
 };
