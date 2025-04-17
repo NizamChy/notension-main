ec0d7dfabffcad1590feb5a3a4f5868811d5389d
NizamChy <nizam.chy.cs@gmail.com>
2025-04-17 13:52:37 +0600

    updated

--- src/components/Grocery/FavoriteSection/FavoriteItem.jsx
+++ src/components/Grocery/FavoriteSection/FavoriteItem.jsx
@@ -1,9 +1,9 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
 import { useSelector } from "react-redux";
 import GroceryItems from "../GroceryItems/GroceryItems";
-import Image from "next/image";
 
 const FavoriteItem = () => {
   const favouriteGroceryItems = useSelector(
@@ -12,11 +12,9 @@ const FavoriteItem = () => {
 
   return (
     <div className="mx-auto px-4 lg:px-24 py-6 mt-12 md:mt-20">
-      {/* {favouriteGroceryItems?.length > 0 && ( */}
       <h1 className="text-lg md:text-xl lg:text-2xl font-semibold lg:font-bold mb-2 md:mb-6 text-deepGray">
         Favourite Grocery Item
       </h1>
-      {/* )}  */}
 
       <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-2 md:gap-5 justify-center items-center">
         {favouriteGroceryItems?.map((item) => (
@@ -31,13 +29,12 @@ const FavoriteItem = () => {
           <div className="min-h-[45vh] flex justify-center items-center w-full">
             <div>
               <Image
-                src="/images/favorite/wishlist.webp"
+                src="/images/favorite/no-wishlist.png"
                 alt="wishlist"
-                height={1200}
-                width={1200}
-                className="object-cover w-40"
+                height={1024}
+                width={1024}
+                className="object-cover w-60"
               />
-
               <p className="text-center text-deepGray font-semibold text-xl">
                 No wishlist yet!
               </p>
