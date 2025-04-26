79b82f45a8c81fa1d9df476c9f1f940185651e03
NizamChy <nizam.chy.cs@gmail.com>
2025-04-26 17:12:44 +0600

    updated

--- src/app/loading.js
+++ src/app/loading.js
@@ -7,7 +7,10 @@ import RoundLoader from "@/utils/round-loader";
 
 const loading = () => {
   return (
-    <div className="min-h-[100vh] flex justify-center items-center">
+    <div
+      className="fixed inset-0 z-50 bg-white flex justify-center items-center"
+      // className="min-h-[100vh] flex justify-center items-center"
+    >
       <RoundLoader />
       {/* <Loader /> */}
       {/* <SingleRoundLoader /> */}
