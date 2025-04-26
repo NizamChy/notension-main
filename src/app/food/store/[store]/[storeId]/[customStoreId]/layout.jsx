a4fea3f4079ff5194f92e0c012402d8edc7ec66b
NizamChy <nizam.chy.cs@gmail.com>
2025-04-26 14:42:21 +0600

    updated

--- src/app/food/store/[store]/[storeId]/[customStoreId]/layout.jsx
+++ src/app/food/store/[store]/[storeId]/[customStoreId]/layout.jsx
@@ -62,27 +62,31 @@ export default function RootLayout({ children }) {
           </div>
 
           <div className="w-full lg:w-[75%] xl:w-[80%] ml-auto md:p-16">
-            {(pathname ===
+            {/* {(pathname ===
               `/food/store/${params?.store}/${params?.storeId}/${params?.customStoreId}` ||
               pathname ===
-                `/food/store/${params?.store}/${params?.storeId}/${params?.customStoreId}/${params?.catId}`) && (
-              <div className="flex flex-col-reverse gap-4 lg:flex-row lg:gap-10">
-                <div className="lg:w-1/2 xl:w-1/3 px-4 lg:px-0">
-                  <ShopInfoCard
-                    onClick={() =>
-                      router.push(
-                        `/food/store/${params?.store}/${params?.storeId}/${params?.customStoreId}`
-                      )
-                    }
-                    shop={visitedFoodStore}
-                    type="food"
-                  />
+                `/food/store/${params?.store}/${params?.storeId}/${params?.customStoreId}/${params?.catId}`) */}
+
+            {!pathname.includes("/orders") &&
+              !pathname.includes("/checkout") &&
+              !pathname.includes("/user") && (
+                <div className="flex flex-col-reverse gap-4 lg:flex-row lg:gap-10">
+                  <div className="lg:w-1/2 xl:w-1/3 px-4 lg:px-0">
+                    <ShopInfoCard
+                      onClick={() =>
+                        router.push(
+                          `/food/store/${params?.store}/${params?.storeId}/${params?.customStoreId}`
+                        )
+                      }
+                      shop={visitedFoodStore}
+                      type="food"
+                    />
+                  </div>
+                  <div className="lg:w-1/2 xl:w-2/3">
+                    <CategoryBannerCarousel />
+                  </div>
                 </div>
-                <div className="lg:w-1/2 xl:w-2/3">
-                  <CategoryBannerCarousel />
-                </div>
-              </div>
-            )}
+              )}
 
             {pathname ===
               `/food/store/${params?.store}/${params?.storeId}/${params?.customStoreId}/${params?.catId}` && (
