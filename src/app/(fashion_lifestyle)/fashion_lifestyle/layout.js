3b2a5bf0ad21fcfc55c34edc5a7e526cce2a94e5
NizamChy <nizam.chy.cs@gmail.com>
2025-11-15 18:03:17 +0600

    updated

--- src/app/(fashion_lifestyle)/fashion_lifestyle/layout.js
+++ src/app/(fashion_lifestyle)/fashion_lifestyle/layout.js
@@ -1,10 +1,8 @@
 import { Toaster } from "react-hot-toast";
 import { Hind_Siliguri } from "next/font/google";
-import ReduxProvider from "@/redux/ReduxProvider";
 import Navbar from "@/components/FashionLifestyle/shared/Navbar/Navbar";
 import Footer from "@/components/FashionLifestyle/shared/Footer/Footer";
 import { CartProvider } from "@/components/FashionLifestyle/context/CartContext";
-import QueryProvider from "@/components/FashionLifestyle/Provider/QueryProvider";
 
 const HindSiliguri = Hind_Siliguri({
   weight: ["300", "400", "500", "600", "700"],
@@ -20,14 +18,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <div className={HindSiliguri.className}>
-      <ReduxProvider>
-        <CartProvider>
-          <Toaster />
-          <Navbar />
-          {children}
-          <Footer />
-        </CartProvider>
-      </ReduxProvider>
+      <CartProvider>
+        <Toaster />
+        <Navbar />
+        {children}
+        <Footer />
+      </CartProvider>
     </div>
   );
 }
