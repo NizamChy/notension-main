54df70afdf9dda1afe1f261180d2433d4839423a
NizamChy <nizam.chy.cs@gmail.com>
2025-11-16 15:02:32 +0600

    wishlist added

--- src/app/(fashion_lifestyle)/fashion_lifestyle/(main)/wishlist/page.jsx
+++ src/app/(fashion_lifestyle)/fashion_lifestyle/(main)/wishlist/page.jsx
@@ -5,13 +5,15 @@ const page = () => {
   return (
     <div className="min-h-content flex justify-center items-center">
       <div>
-        <Image
-          className="w-full"
-          src="/images/empty-wishlist.webp"
-          alt="empty-wishlist"
-          height={500}
-          width={750}
-        />
+        <div className="max-w-md">
+          <Image
+            className="w-full"
+            src="/images/fashion-lifestyle/empty-wishlist.webp"
+            alt="empty-wishlist"
+            height={500}
+            width={750}
+          />
+        </div>
 
         <p className="text-2xl text-primary text-center font-medium">
           Your wishlist is empty!
