a2c02ef7015c8ee64eec4e7df4d85c5f94ab76ad
NizamChy <nizam.chy.cs@gmail.com>
2025-11-16 16:20:36 +0600

    removed unused files

--- src/app/get-mobile-location/page.jsx
+++ src/app/get-mobile-location/page.jsx
@@ -1,12 +1,9 @@
 import React from "react";
-// import Map from "@/components/shared/Map/Map";
 import MapMobile from "@/components/shared/Map/MapMobile";
 
 const page = () => {
   return (
     <div className="mt-4 block md:hidden">
-      {/* <Map /> */}
-
       <MapMobile />
     </div>
   );
--- src/components/shared/Map/MapMobile.jsx
+++ src/components/shared/Map/MapMobile.jsx
@@ -211,18 +211,12 @@ const MapMobile = () => {
 
   if (isMapLoading) {
     return (
-      <div className="relative animate-pulse">
-        {/* Search input skeleton */}
+      <div className="relative animate-pulse w-[90%]">
         <div className="h-12 bg-gray-200 rounded mb-4"></div>
-
-        {/* Map container skeleton */}
         <div className="h-[75vh] md:h-[55vh] bg-gray-200 rounded"></div>
-
-        {/* Button skeleton */}
         <div className="hidden md:flex justify-center gap-2 mt-4">
           <div className="h-12 bg-gray-200 rounded w-full"></div>
         </div>
-
         <div className="flex md:hidden justify-center gap-2 mt-4">
           <div className="h-12 bg-gray-200 rounded w-full"></div>
         </div>
