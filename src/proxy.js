10c0c0e12d2acaa8731b46de558d356936b721fb
NizamChy <nizam.chy.cs@gmail.com>
2025-12-06 16:31:36 +0600

    proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export function middleware(request) {
+export function proxy(request) {
   const userCookie = request.cookies.get("user_info")?.value;
 
   let user = null;
