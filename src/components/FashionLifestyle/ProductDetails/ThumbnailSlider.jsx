2f4da3bf1e49b3fb02c3a948adb2a4df7d16199c
NizamChy <nizam.chy.cs@gmail.com>
2025-12-23 12:07:04 +0600

    updated

--- src/components/FashionLifestyle/ProductDetails/ThumbnailSlider.jsx
+++ src/components/FashionLifestyle/ProductDetails/ThumbnailSlider.jsx
@@ -5,44 +5,65 @@ import { Navigation } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { FASHION_IMAGE_URL } from "@/api-endpoints/secret";
 
-import "swiper/css";
-import "swiper/css/navigation";
-
 const ThumbnailSlider = ({
   detailImages,
   setCurrentImageIndex,
   currentImageIndex,
   product,
 }) => {
+  // Prevent event bubbling for touch events
+  const handleTouchStart = (e) => {
+    e.stopPropagation();
+  };
+
+  const handleTouchMove = (e) => {
+    e.stopPropagation();
+  };
+
+  const handleTouchEnd = (e) => {
+    e.stopPropagation();
+  };
+
   return (
-    <Swiper
-      modules={[Navigation]}
-      navigation
-      slidesPerView={4}
-      spaceBetween={10}
-      className="thumbnail-swiper"
-      touchStartPreventDefault={false}
-    >
-      {detailImages.map((img, index) => (
-        <SwiperSlide key={index}>
-          <button
-            onClick={() => setCurrentImageIndex(index)}
-            className={`w-full aspect-square bg-gray-100 rounded overflow-hidden border ${
-              currentImageIndex === index
-                ? "border-indigo-500"
-                : "border-transparent"
-            }`}
-          >
-            <img
-              src={`${FASHION_IMAGE_URL}/${img}`}
-              alt={`${product?.product_title_eng} thumbnail ${index + 1}`}
-              className="w-full h-full object-cover select-none pointer-events-none"
-              draggable={false}
-            />
-          </button>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      {/* Swiper Thumbnail Gallery */}
+      <Swiper
+        modules={[Navigation]}
+        navigation
+        slidesPerView={4}
+        spaceBetween={10}
+        className="thumbnail-swiper"
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={handleTouchEnd}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {detailImages?.map((img, index) => (
+          <SwiperSlide key={index}>
+            <div className="p-1">
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  setCurrentImageIndex(index);
+                }}
+                onTouchStart={handleTouchStart}
+                onTouchMove={handleTouchMove}
+                onTouchEnd={handleTouchEnd}
+                className={`w-full aspect-square bg-gray-100 rounded overflow-hidden ${
+                  currentImageIndex === index ? "ring-2 ring-indigo-500" : ""
+                }`}
+              >
+                <img
+                  src={`${FASHION_IMAGE_URL}/${img}`}
+                  alt={`${product?.product_title_eng} thumbnail ${index + 1}`}
+                  className="w-full h-full object-cover"
+                />
+              </button>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
 
